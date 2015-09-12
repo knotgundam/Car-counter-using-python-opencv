3f344d20f6780e6f9ba009fc4fc2be38c0195169
knotgundam <matthanaphaisarn@gmail.com>
2015-09-12 17:36:20 +0700

    first commit

---
+++ BgLearning.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+
+video = cv2.VideoCapture('vdo/Export Folder(1)/fix01.avi')
+# BackgroundSubtractorMOG(history=None, nmixtures=None, backgroundRatio=None, noiseSigma=None)
+fgbg = cv2.BackgroundSubtractorMOG()
+
+while(video.isOpened()):
+    ret, frame = video.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    fgmask = fgbg.apply(gray)
+    # cv2.imshow('frame',gray)
+    cv2.imshow('frame',fgmask)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
+# BackgroundSubtractorMOG()
+
+
